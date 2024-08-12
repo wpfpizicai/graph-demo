27b81ad6edf694543365bc7a103d88cce183e35b
王鹏飞 <444689899@qq.com>
2024-08-13 00:11:08 +0800

    本地模拟

--- graph.js
+++ graph.js
@@ -1,17 +1,93 @@
-const graph = new G6.Graph({
-    fitView: true,
-    fitViewPadding: [20, 40, 50, 20],
-    container: 'mountNode', // String | HTMLElement，必须，在 Step 1 中创建的容器 id 或容器本身
-    width: 1000, // Number，必须，图的宽度
-    height: 1000, // Number，必须，图的高度
-    layout: {
-      // Object，可选，布局的方法及其配置项，默认为 random 布局。
-      type: 'force', // 指定为力导向布局
-      preventOverlap: true, // 防止节点重叠
-      linkDistance: 100, // 指定边距离为100
-      nodeSize: 30        // 节点大小，用于算法中防止节点重叠时的碰撞检测。由于已经在上一节的元素配置中设置了每个节点的 size 属性，则不需要在此设置 nodeSize。
-    },
-  });
-  
-  graph.data(data); // 读取 Step 2 中的数据源到图上
-  graph.render(); //
+
+const width = 928;
+const height = 680;
+// Specify the color scale.
+const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+
+// The force simulation mutates links and nodes, so create a copy
+// so that re-evaluating this cell produces the same result.
+const links = data.links.map(d => ({...d}));
+const nodes = data.nodes.map(d => ({...d}));
+
+// Create a simulation with several forces.
+const simulation = d3.forceSimulation(nodes)
+    .force("link", d3.forceLink(links).id(d => d.id))
+    .force("charge", d3.forceManyBody())
+    .force("x", d3.forceX())
+    .force("y", d3.forceY());
+
+// Create the SVG container.
+const svg = d3.select("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", [-width / 2, -height / 2, width, height])
+    .attr("style", "max-width: 100%; height: auto;");
+
+// Add a line for each link, and a circle for each node.
+const link = svg.append("g")
+    .attr("stroke", "#999")
+    .attr("stroke-opacity", 0.6)
+  .selectAll("line")
+  .data(links)
+  .join("line")
+    .attr("stroke-width", d => Math.sqrt(d.value));
+
+const node = svg.append("g")
+    .attr("stroke", "#fff")
+    .attr("stroke-width", 1.5)
+  .selectAll("circle")
+  .data(nodes)
+  .join("circle")
+    .attr("r", d=>d.radius * 5)
+    .attr("fill", d => color(d.group));
+
+  node.append('text')
+    .attr('dx', 6)
+    .attr('dy', 3)
+    .text(d => d.label);
+
+// Add a drag behavior.
+node.call(d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended));
+
+// Set the position attributes of links and nodes each time the simulation ticks.
+simulation.on("tick", () => {
+  link
+      .attr("x1", d => d.source.x)
+      .attr("y1", d => d.source.y)
+      .attr("x2", d => d.target.x)
+      .attr("y2", d => d.target.y);
+
+  node
+      .attr("cx", d => d.x)
+      .attr("cy", d => d.y);
+});
+
+// Reheat the simulation when drag starts, and fix the subject position.
+function dragstarted(event) {
+  if (!event.active) simulation.alphaTarget(0.3).restart();
+  event.subject.fx = event.subject.x;
+  event.subject.fy = event.subject.y;
+}
+
+// Update the subject (dragged node) position during drag.
+function dragged(event) {
+  event.subject.fx = event.x;
+  event.subject.fy = event.y;
+}
+
+// Restore the target alpha so the simulation cools after dragging ends.
+// Unfix the subject position now that it’s no longer being dragged.
+function dragended(event) {
+  if (!event.active) simulation.alphaTarget(0);
+  event.subject.fx = null;
+  event.subject.fy = null;
+}
+
+// When this cell is re-run, stop the previous simulation. (This doesn’t
+// really matter since the target alpha is zero and the simulation will
+// stop naturally, but it’s a good practice.)
+invalidation.then(() => simulation.stop());
---
+++ phy.js
@@ -0,0 +1,79 @@
+const data = {
+  "nodes":[
+    {
+      "id": "1",
+      "group": "A",
+      "label": "驻波",
+      "radius": 4
+    },
+    {
+      "id": "2",
+      "group": "B",
+      "label": "科学应用",
+      "radius": 1
+    },
+    {
+      "id": "3",
+      "group": "C",
+      "label": "火箭发射",
+      "radius": 2
+    },
+    {
+      "id": "4",
+      "group": "C",
+      "label": "工程制造",
+      "radius": 2
+    },
+    {
+      "id": "5",
+      "group": "B",
+      "label": "商业开发",
+      "radius": 2
+    },
+    {
+      "id": "6",
+      "group": "C",
+      "label": "地理开发",
+      "radius": 2
+    },
+    {
+      "id": "7",
+      "group": "C",
+      "label": "学习制造",
+      "radius": 2
+    }
+
+  ],
+  "links":[
+    {
+      "source": "1",
+      "target": "2",
+      "value": 40
+    },
+    {
+      "source": "2",
+      "target": "3",
+      "value": 2
+    },
+    {
+      "source": "2",
+      "target": "4",
+      "value": 2
+    },
+    {
+      "source": "1",
+      "target": "5",
+      "value": 4
+    },
+    {
+      "source": "5",
+      "target": "6",
+      "value": 2
+    },
+    {
+      "source": "5",
+      "target": "7",
+      "value": 2
+    }
+  ]
+}
